c55d90a13254d8f8d93145e6a52e7335bc5cb602
Kim-MyungJin <myungjin.kim@switchwon.com>
2025-05-26 10:11:17 +0900

    tsup eslint ignores 적용

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,4 +11,7 @@ export default tseslint.config(
       '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
+  {
+    ignores: ['node_modules', 'dist', 'build'],
+  },
 )
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import { add } from './add'
+
+const hello: string = 'hello'
+
+console.log(hello)
+
+const result = add(2, 5)
+console.log(`result: ${result}`)
